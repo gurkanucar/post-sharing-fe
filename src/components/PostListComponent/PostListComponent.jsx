79fca4796937c51e6615da263edf560b5066c4b7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-17 02:45:52 +0300

    loading text implemented

--- src/components/PostListComponent/PostListComponent.jsx
+++ src/components/PostListComponent/PostListComponent.jsx
@@ -3,17 +3,22 @@ import { getAllPosts } from "../../api/apiCalls";
 import PostListItem from "./PostListItem";
 
 import "./PostListComponent.css";
+import { Spinner } from "react-bootstrap";
 
 const PostListComponent = (props) => {
   const { posts, activeUser } = props;
 
   return (
     <div className="postListComponent">
-      {posts
-        .sort((a, b) => new Date(b.created) - new Date(a.created))
-        .map((post, index) => (
-          <PostListItem key={post.id} post={post} activeUser={activeUser} />
-        ))}
+      {posts.length > 0 ? (
+        posts
+          .sort((a, b) => new Date(b.created) - new Date(a.created))
+          .map((post, index) => (
+            <PostListItem key={post.id} post={post} activeUser={activeUser} />
+          ))
+      ) : (
+        <h1 style={{ marginTop: 50 }}>Loading...</h1>
+      )}
     </div>
   );
 };
