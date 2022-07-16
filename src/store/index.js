101e3a46918d0904c2768ba1ce586f51671b9853
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-17 02:24:55 +0300

    redux index updated

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 
 import auth from "./auth";
+import deliveredNotifs from "./deliveredNotifs";
 
 const reducer = combineReducers({
   auth,
+  deliveredNotifs,
 });
 
 const store = configureStore({
